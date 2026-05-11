10d7af6a29316e9d71a955c932c92d4402396653
“Bogdan <bogdan.daniel.ioan@outlook.com>
2026-05-11 21:11:46 +0300

    fix

--- ReteaDeSocializare/src/main/java/app/security/UserDetailsImpl.java
+++ ReteaDeSocializare/src/main/java/app/security/UserDetailsImpl.java
@@ -2,14 +2,16 @@ package app.security;
 
 
 import app.users.repository.UserRepository;
+import org.springframework.stereotype.Service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 
-
+@Service
 public class UserDetailsImpl implements UserDetailsService {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
     public UserDetailsImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
